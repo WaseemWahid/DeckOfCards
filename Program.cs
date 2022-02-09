f6c6ca298bb337c1c0bfd3c5e117288b45c66138
Waseem Wahid <wahid.waseem@gmail.com>
2022-02-09 10:20:56 -0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DeckOfCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck deck = new Deck ();
+
+            Player waseem = new Player("Waseem");
+
+            deck.Shuffle();
+
+            waseem.Draw(deck, waseem);
+
+            deck.Shuffle();
+
+            waseem.Draw(deck, waseem);
+
+            Console.WriteLine(waseem.Hand);
+
+        }
+    }
+}
